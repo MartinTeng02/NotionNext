ba18853d68fe0b2ae337ee5b415b9dbbaf8356aa
MartinTeng02 <156582129+MartinTeng02@users.noreply.github.com>
2024-07-09 20:27:09 +0800

    Update Announcement.js

--- themes/medium/components/Announcement.js
+++ themes/medium/components/Announcement.js
@@ -4,7 +4,7 @@ import dynamic from 'next/dynamic'
 const NotionPage = dynamic(() => import('@/components/NotionPage'))
 
 const Announcement = ({ post, className }) => {
-   const { locale } = useGlobal()
+//   const { locale } = useGlobal()
   if (post?.blockMap) {
     return <div className={className}>
         <section id='announcement-wrapper' className="dark:text-gray-300 rounded-xl px-2 py-4">
